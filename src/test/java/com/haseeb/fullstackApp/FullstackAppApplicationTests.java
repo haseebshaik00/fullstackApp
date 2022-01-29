83dbb999f9f58b12c819571126151360af9ed544
Shaik Haseeb Ur Rahman <haseebshaik00@gmail.com>
2022-01-30 03:10:17 +0530

    generated bootstrap of the repo

---
+++ src/test/java/com/haseeb/fullstackApp/FullstackAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.haseeb.fullstackApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullstackAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
